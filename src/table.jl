0f5941e3b252c761e8fd096b0d887af546e6f16a
David Gold <david.gold.a@gmail.com>
2015-08-06 13:53:03 -0700

    Make `Column`s own `Table`s via `(tab::Table).id`

--- src/table.jl
+++ src/table.jl
@@ -1,107 +1,31 @@
 
-# immutable Table{C} # <: AbstractArray{Any, 2}
-#     columns::C
-#     colnames::Vector{UTF8String}
-# end
-
-# function Table(curs::Cursor)
-#     env(curs)
-#     cols = materialize!(curs)
-#     return Table{typeof(cols)}(cols, curs.colnames)
-# end
-
 immutable Column{T}
-    # idx::Int
-    colname::UTF8String
-    # col::Dict{Symbol, Vector{T}}
+    colsymb::Symbol
     cols::Vector{Vector{T}}
 end
 
+Base.length(column::Column) = length(column.cols[env_id[1]])
+Base.setindex!(columm::Column, x, i) = error()
+
 immutable Table <: AbstractArray{Any, 2}
-    columns::Vector{Column}
-    # id::Symbol
+    columns::Vector{Tuple{Symbol, Column}}
     id::UInt16
     nrows::Int
-    # colnames::Vector{UTF8String}
 
     let count = UInt16(1)
         global Table
         function Table(curs::Cursor)
-            cols, nrows = materialize!(curs)
-            res = new(Vector{Column}(), count, nrows)
-            env!(curs, cols, res)
+            cols, nrows, colsymbs = materialize!(curs)
+            # res = new(Vector{Column}(), count, nrows)
+            res = new(Vector{Tuple{Symbol, Column}}(), count, nrows)
+            env!(res, cols, colsymbs, curs.coltypes)
             count += UInt16(1)
             return res
         end
     end
 end
 
-
-# function env(curs::Cursor)
-#     # colinds = Vector{Column}()
-#     for (i, colname) in enumerate(curs.colnames)
-#         typ = curs.coltypes[i]
-#         current = current_module()
-#         eval(current, :( global const $(symbol(colname)) = Nierika.Column{$typ}($i, $colname) ))
-#         # eval(current, :( global const $(symbol(colname)) = Nierika.Column{$typ}($i) ))
-#         # push!(colinds, Column{typ}(i))
-#
-#     end
-#     # return colinds
-# end
-
-function env!(curs::Cursor, cols, tab::Table)
-    for (i, colname) in enumerate(curs.colnames)
-        typ = curs.coltypes[i]
-        current = current_module()
-        colsymb = symbol(colname)
-        _colsymb = Expr(:quote, colsymb)
-        _tabid = Expr(:quote, tab.id)
-        eval(current, quote
-            if !in($_colsymb, names($current))
-                # coldict = Dict{Symbol, Array{$typ}}( $_tabid => $(cols[i]) )
-                coldict = Vector{$typ}[ $(cols[i]) ]
-                # global const $colsymb = Nierika.Column{$typ}($i, $colname, coldict)
-                global const $colsymb = Nierika.Column{$typ}($colname, coldict)
-            else
-                # $colsymb.cols[$_tabid] = $(cols[i])
-                push!($colsymb.cols, $(cols[i]))
-            end
-            push!($(tab.columns), $colsymb)
-        end)
-    end
-end
-
-Base.size(tab::Table) = (nrows(tab), length(tab.columns))
-# Base.size(tab::Table) = size(tab.columns)
-Base.ndims(tab::Table) = 2
-nrows(tab::Table) = tab.nrows
-
-
-Base.linearindexing(tab::Table) = Base.LinearSlow()
-
-function Base.getindex(tab::Table, i::Int)
-    sub = ind2sub(size(tab), i)
-    getindex(tab, sub...)
-end
-
-@inline function Base.getindex(tab::Table, i::Int, j::Int)
-    return tab.columns[j].cols[tab.id][i]
-    # return tab.columns[j].cols[1][i]
-end
-
-# @inline function Base.getindex(tab::Table, i::Int, j::Int)
-#     return tab.columns[i, j]
-# end
-
-# @inline function Base.getindex{T}(tab::Table, i::Int, col::Column{T})
-#     return tab.columns[i, col.idx]::T
-# end
-
-@inline function Base.getindex{T}(tab::Table, i::Int, column::Column{T})
-    return column.cols[tab.id][i]
-    # return column.cols[1][i]
-end
+# Table construction utilities
 
 function sqlite3_get(::Type{Int}, stmt, i)
     return sqlite3_column_int64(stmt.handle, i-1)::Int
@@ -134,22 +58,80 @@ function materialize!(curs::Cursor)
         status = execute!(stmt)
     end
     curs.status = status
-    return cols, length(cols[1])
-    # return tuple(cols...)
-    # return hcat(cols...)
+    colsymbs = [ symbol(curs.colnames[i]) for i in 1:n ]
+    res = Dict{Symbol, Array}([ colsymbs[i] => cols[i] for i in 1:n ])
+    return res, length(cols[1]), colsymbs
 end
 
+# Keep track of all column names we have generated for a given module
+let registry = Dict{Module, Vector{Symbol}}()
+    global env!
+    function env!(tab::Table, cols, colsymbs, coltypes)
+        current = current_module()
+        current_cols = get!(registry, current, Vector{Symbol}())
+        for (i, colsymb) in enumerate(colsymbs)
+            _colsymb = Expr(:quote, colsymb)
+            typ = coltypes[i]
+            # If the current module does not have the column name in its namespace,
+            # create a Column{typ} with said name and add the name to the registry
+            eval(current, quote
+                if !in($_colsymb, names($current))
+                    colvec = Vector{$typ}[]
+                    global const $colsymb = Nierika.Column{$typ}($_colsymb, colvec)
+                    push!($current_cols, $_colsymb)
+                end
+            end)
+        end
+        current_cols = registry[current]
+        # For each Column name in the registry, if that name belongs to `tab`,
+        # then push to that Column's `cols` field the appropriate Vector{typ};
+        # otherwise, push any empty vector.
+        for (i, colsymb) in enumerate(current_cols)
+            _colsymb = Expr(:quote, colsymb)
+            if in(colsymb, colsymbs)
+                eval(current, quote
+                    push!($colsymb.cols, $cols[$_colsymb])
+                    # $tab.columns[$_colsymb] = $colsymb
+                    push!($tab.columns, ($_colsymb, $colsymb))
+                end)
+            else
+                eval(current, quote
+                    push!($colsymb.cols, [])
+                end)
+            end
+        end
+    end
+end # let registry
 
-# const safety_id = gensym("UNSAFE_ENV")
-# const env_id = Symbol[safety_id]
+# primitives and indexing
+
+Base.size(tab::Table) = (nrows(tab), length(tab.columns))
+Base.ndims(tab::Table) = 2
+nrows(tab::Table) = tab.nrows
+
+Base.linearindexing(tab::Table) = Base.LinearSlow()
+
+function Base.getindex(tab::Table, i::Int)
+    sub = ind2sub(size(tab), i)
+    getindex(tab, sub...)
+end
+
+@inline function Base.getindex(tab::Table, i::Int, j::Int)
+    return tab.columns[j][2].cols[tab.id][i]
+end
+
+@inline function Base.getindex{T}(tab::Table, i::Int, column::Column{T})
+    return column.cols[tab.id][i]
+end
+
 const safety_id = UInt16(0)
 const env_id = UInt16[safety_id]
 
-function Base.getindex{T}(column::Column{T}, i::Int)
+@inline function Base.getindex{T}(column::Column{T}, i::Int)
     return column.cols[env_id[1]][i]
 end
 
-Base.length(column::Column) = length(column.cols[env_id[1]])
+# "environment reification"
 
 function with(f::Function, tab::Table)
     env_id[1] = tab.id
